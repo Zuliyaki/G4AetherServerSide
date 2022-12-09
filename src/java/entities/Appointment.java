a65343bf925f31d4883e2069e9dbdfa9c8ff7fdc
janam1003 <lucasjanamsmile@gmail.com>
2022-12-09 10:36:35 +0100

    Entity Appointment

--- src/java/entities/Appointment.java
+++ src/java/entities/Appointment.java
@@ -1,11 +1,30 @@
 package entities;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-public class Appointment {
-    private Date appointmentDate;
+@Entity
+@Table(name = "appointments", schema = "aether")
+public class Appointment implements Serializable {
+    
+    @Id 
+    @Temporal(TemporalType.TIMESTAMP)
     private Boolean appointmentChange;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date appointmentDate;
+    
+    @ManyToOne
     private Patient patient;
+    
+    @OneToMany(mappedBy="patient")
     private Psychologist psychologist;
 
     /**
